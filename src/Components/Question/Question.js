464f03893b1bb72ccc576b6c336a917d1d365cc7
RobinTheRedLight <robinmitra789@gmail.com>
2022-10-16 18:04:41 +0600

    Toast Added

--- src/Components/Question/Question.js
+++ src/Components/Question/Question.js
@@ -1,18 +1,38 @@
 import React, { useState } from 'react';
 import './Question.css'
 import { EyeIcon } from '@heroicons/react/24/solid';
-import { Button, Form } from 'react-bootstrap';
+import { Form } from 'react-bootstrap';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 const Question = ({ questioN }) => {
-    console.log(questioN);
     const { question, correctAnswer, options } = questioN;
     const questionStr = question.replace(/<\/?[^>]+(>|$)/g, '');
     const [alertMaker, setAlertMaker] = useState(correctAnswer);
     const answerAlert = answer => {
         if (alertMaker === answer) {
-            alert('Right Answer');
+            toast.success('🦄 Right Answer!', {
+                position: "top-right",
+                autoClose: 1000,
+                hideProgressBar: false,
+                closeOnClick: false,
+                pauseOnHover: false,
+                draggable: false,
+                progress: undefined,
+                theme: "colored",
+            });
         }
         else {
-            alert('Wrong Answer')
+            toast.error('Wrong Answer!', {
+                position: "top-right",
+                autoClose: 1000,
+                hideProgressBar: false,
+                closeOnClick: false,
+                pauseOnHover: false,
+                draggable: false,
+                progress: undefined,
+                theme: "colored",
+            });
         }
     }
     return (
@@ -23,18 +43,34 @@ const Question = ({ questioN }) => {
                         <h4>{questionStr}</h4>
                     </div>
                     <div className="iconContainer">
-                        <EyeIcon className="icon" />
+                        <EyeIcon  className="icon" />
                     </div>
                 </div>
                 <div className="options">
                     <Form.Group >
                         {
                             options.map(option =>
-                                <Form.Check
-                                    key={option} onClick={() => answerAlert(option)} className='option' name='question' label={option} id={option} type='radio'>
-                                </Form.Check>)
+                                <div key={option}>
+                                    <Form.Check
+                                        onClick={() => answerAlert(option)} className='option' name='question' label={option} id={option} type='radio'>
+                                    </Form.Check>
+                                    <ToastContainer
+                                        position="top-right"
+                                        autoClose={1000}
+                                        hideProgressBar={false}
+                                        newestOnTop={false}
+                                        closeOnClick
+                                        rtl={false}
+                                        pauseOnFocusLoss
+                                        draggable={false}
+                                        pauseOnHover={false}
+                                        theme="colored"
+                                    />
+                                </div>
+                            )
                         }
                     </Form.Group>
+
                 </div>
             </div>
         </div >
